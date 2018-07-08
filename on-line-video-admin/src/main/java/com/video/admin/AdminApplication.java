539733b702f13f3dc17b2c7eea29939425872d27
master <qinyuchu.huantian@gmail.com>
2018-07-08 20:54:37 +0800

    添加Admin健康监控

--- on-line-video-admin/src/main/java/com/video/admin/AdminApplication.java
+++ on-line-video-admin/src/main/java/com/video/admin/AdminApplication.java
@@ -1,18 +1,18 @@
-package com.video.zuul;
+package com.video.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
-@EnableZuulProxy
-public class ZuulApplication {
+@EnableAdminServer
+public class AdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulApplication.class, args);
+		SpringApplication.run(AdminApplication.class, args);
 	}
 }
